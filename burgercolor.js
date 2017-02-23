6c1a4b102a634f61422dc193983d5dc8dc89b550
Casper Panduro <casperpanduro@Caspers-iMac.local>
2017-02-23 11:30:33 +0100

    comments removed

--- burgercolor.js
+++ burgercolor.js
@@ -45,7 +45,7 @@
 							else {
 								next = colorarray[i+1]['position'];	
 							}
-							
+
 
 							if (scrollTop > colorarray[i]['position'] && scrollTop < next) {
 								var st = $(this).scrollTop();
@@ -64,7 +64,7 @@
 						    	}
 							    
 							    lastScrollTop = st;
-							    console.log('now: '+colorarray[i]['background']+'. next/prev: '+colorarray[pre]['background']);
+							    //console.log('now: '+colorarray[i]['background']+'. next/prev: '+colorarray[pre]['background']);
 
 							    if(colorarray[i]['background'] != colorarray[pre]['background']) {
 							    	// console.log(colorarray[i]['background']);
@@ -111,35 +111,7 @@
 			    }
 		    }
 		  
-		  // $.fn.lightOrDark = function(){
-		  //   var r,b,g,hsp
-		  //     , a = this.css('background-color');
-		    
-		  //   if (a.match(/^rgb/)) {
-		  //     a = a.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
-		  //     r = a[1];
-		  //     g = a[2];
-		  //     b = a[3];
-		  //   } else {
-		  //     a = +("0x" + a.slice(1).replace( // thanks to jed : http://gist.github.com/983661
-		  //         a.length < 5 && /./g, '$&$&'
-		  //       )
-		  //     );
-		  //     r = a >> 16;
-		  //     b = a >> 8 & 255;
-		  //     g = a & 255;
-		  //   }
-		  //   hsp = Math.sqrt( // HSP equation from http://alienryderflex.com/hsp.html
-		  //     0.299 * (r * r) +
-		  //     0.587 * (g * g) +
-		  //     0.114 * (b * b)
-		  //   );
-		  //   if (hsp>127.5) {
-		  //     this.attr('data-bg', 'light');
-		  //   } else {
-		  //     this.attr('data-bg', 'dark');
-		  //   }
-		  // }
+		  
 		  
 		})(jQuery);
 
